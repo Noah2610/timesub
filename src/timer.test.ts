26bcf89d10289bda84a4bbd8f582b75fdb6abed9
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-06-02 12:08:54 +0200

    Add subscribe unit tests

--- src/timer.test.ts
+++ src/timer.test.ts
@@ -1,4 +1,4 @@
-import { createTimer } from ".";
+import { createTimer, Timer } from ".";
 
 describe("createTimer", () => {
     it("creates a timer", () => {
@@ -46,4 +46,55 @@ describe("timer API", () => {
         timer.reset();
         expect(timer.time).toBe(0.0);
     });
+});
+
+describe("subscribe to timer", () => {
+    it("subscribes and calls subscription function", () => {
+        const timer = createTimer();
+
+        const callback = jest.fn((_: Timer) => {});
+        timer.subscribe(callback);
+
+        timer.play();
+        timer.pause();
+
+        expect(callback).toHaveBeenCalledTimes(2);
+    });
+
+    it("subscribes and unsubscribes", () => {
+        const timer = createTimer();
+
+        const callback = jest.fn((_: Timer) => {});
+        const unsubscribe = timer.subscribe(callback);
+
+        timer.play();
+        expect(callback).toHaveBeenCalledTimes(1);
+
+        unsubscribe();
+        timer.pause();
+        expect(callback).toHaveBeenCalledTimes(1);
+    }),
+        it("finishes timer", (done) => {
+            const timer = createTimer({
+                duration: 100,
+                updateInterval: 50,
+            });
+
+            const expectedTimesCalled = 3;
+            let timesCalled = 0;
+
+            const callback = jest.fn(({ isPlaying }: Timer) => {
+                timesCalled++;
+                const expectedIsPlaying =
+                    timesCalled < expectedTimesCalled ? true : false;
+                expect(isPlaying).toBe(expectedIsPlaying);
+                if (timesCalled === expectedTimesCalled) {
+                    expect(callback).toHaveBeenCalledTimes(expectedTimesCalled);
+                    done();
+                }
+            });
+            timer.subscribe(callback);
+
+            timer.play();
+        }, 300);
 });
