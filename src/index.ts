e79e192adac1061d0fe9877993b20623d42ee192
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-06-04 01:07:23 +0200

    Refactor
    Move all types into new `types.ts` module.
    Add API creator functions.
    Add `InternalApi` type and object.

--- src/index.ts
+++ src/index.ts
@@ -1,148 +1,123 @@
-/**
- * The Timer context, containing both the state and API functions.
- */
-export type Timer = TimerState & TimerApi;
+import {
+    Timer,
+    TimerApi,
+    TimerEvent,
+    TimerOptions,
+    TimerState,
+    TimerSubscriber,
+} from "./types";
 
-/**
- * Timer state values.
- */
-export interface TimerState {
-    /**
-     * Current playback time.
-     */
-    time: number;
-
-    /**
-     * Current playback status.
-     */
-    isPlaying: boolean;
-
-    /**
-     * If you've set a duration, then this could be `true`
-     * if the time has reached the duration.
-     */
-    isFinished: boolean;
-}
-
-/**
- * Timer API functions, to control playback and subscribe to state changes.
- */
-export interface TimerApi {
-    /**
-     * Get the current time.
-     */
-    getTime(): number;
-
-    /**
-     * Set the time to the given `time` in milliseconds.
-     */
-    setTime(time: number): void;
-
-    /**
-     * Play the timer, if it's paused.
-     */
-    play(): boolean;
-
-    /**
-     * Pause the timer.
-     */
-    pause(): boolean;
-
-    /**
-     * Toggle between playing and paused states.
-     */
-    togglePlay(): boolean;
-
-    /**
-     * Resets the timer time to 0, and stops the timer if it was playing.
-     * All subscriptions will be kept.
-     */
-    reset(): void;
-
-    /**
-     * Get playback status.
-     */
-    getIsPlaying(): boolean;
-
-    /**
-     * Subscribe to time updates.
-     * The given callback function will be called anytime the state changes.
-     * The callback gets two arguments:
-     *   1. The `Timer` context, including state and API functions,
-     *   2. An `TimerEvent` object, that contains a `type` property,
-     *      informing what caused the update.
-     * Returns a function that can be called to unsubscribe.
-     */
-    subscribe: TimerApiSubscribe;
-}
-
-export type TimerApiSubscribe = (cb: TimerSubscriber) => () => void;
-
-export type TimerSubscriber = (timer: Timer, event: TimerEvent) => void;
-
-/**
- * An object passed as the second argument to a subscriber callback.
- */
-export type TimerEvent =
-    /** When the `play()` function triggered the update. */
-    | {
-          type: "play";
-      }
-    /** When the `pause()` function triggered the update. */
-    | {
-          type: "pause";
-      }
-    /** When the `setTime()` function triggered the update. */
-    | {
-          type: "setTime";
-      }
-    /** When the `reset()` function triggered the update. */
-    | {
-          type: "reset";
-      }
-    /** When it's updated by the interval. */
-    | {
-          type: "update";
-      }
-    /** When the timer finishes. */
-    | {
-          type: "finish";
-      };
-
-/**
- * Options you can pass to the `createTimer` function.
- */
-export interface TimerOptions {
-    /**
-     * The maximum duration for the timer in milliseconds.
-     * Once the time has reached the duration, the timer will stop.
-     * If "infinite", the timer will never stop on its own.
-     *
-     * Default: "infinite"
-     */
-    duration: number | "infinite";
-
-    /**
-     * The delay in milliseconds between every update.
-     * Time itself is always calculated with `Date`, this is just
-     * how often it recalculates and calls all subscriber functions.
-     *
-     * Default: 100
-     */
-    updateInterval: number;
-}
+export * from "./types";
 
 interface TimerInternalState {
     timeout: NodeJS.Timeout | undefined;
     lastUpdate: number | undefined;
     subscribers: Record<number, TimerSubscriber>;
     nextSubscriberIdx: number;
+    timer: Timer | undefined;
+}
+
+interface TimerInternalApi {
+    createTimeout(): NodeJS.Timeout;
+    update(): void;
+    updateTime(): void;
+    updateSubscribers(event: TimerEvent): void;
 }
 
 const DEFAULT_TIMER_OPTIONS: TimerOptions = {
     duration: "infinite",
     updateInterval: 100,
 };
+
+type CreateApiFn<T extends keyof TimerApi, TParams = undefined> =
+    TParams extends undefined
+        ? (
+              state: TimerState,
+              internalState: TimerInternalState,
+              internalApi: TimerInternalApi,
+          ) => TimerApi[T]
+        : (
+              state: TimerState,
+              internalState: TimerInternalState,
+              internalApi: TimerInternalApi,
+              extraParams: TParams,
+          ) => TimerApi[T];
+
+const createApiPlay: CreateApiFn<"play"> =
+    (state, internalState, internalApi) => () => {
+        if (state.isPlaying || state.isFinished) {
+            return false;
+        }
+
+        state.isPlaying = true;
+        internalState.timeout !== undefined &&
+            clearTimeout(internalState.timeout);
+        internalState.timeout = internalApi.createTimeout();
+        internalApi.updateTime();
+        internalApi.updateSubscribers({ type: "play" });
+
+        return true;
+    };
+
+const createApiPause: CreateApiFn<"pause"> =
+    (state, internalState, internalApi) => () => {
+        if (!state.isPlaying || state.isFinished) {
+            return false;
+        }
+
+        state.isPlaying = false;
+        internalState.timeout !== undefined &&
+            clearTimeout(internalState.timeout);
+        internalApi.updateTime();
+        internalApi.updateSubscribers({ type: "pause" });
+        internalState.lastUpdate = undefined;
+
+        return true;
+    };
+
+const createApiTogglePlay: CreateApiFn<
+    "togglePlay",
+    { play: () => boolean; pause: () => boolean }
+> =
+    (state, _internalState, _internalApi, { play, pause }) =>
+    () =>
+        state.isPlaying ? pause() : play();
+
+const createApiReset: CreateApiFn<"reset"> =
+    (state, internalState, internalApi) => () => {
+        internalState.timeout !== undefined &&
+            clearTimeout(internalState.timeout);
+        internalState.lastUpdate = undefined;
+        state.time = 0;
+        state.isPlaying = false;
+        state.isFinished = false;
+        internalApi.updateSubscribers({ type: "reset" });
+    };
+
+const createApiGetTime: CreateApiFn<"getTime"> =
+    (state, _internalState, _internalApi) => () =>
+        state.time;
+
+const createApiSetTime: CreateApiFn<"setTime"> =
+    (state, internalState, internalApi) => (time) => {
+        internalState.timeout !== undefined &&
+            clearTimeout(internalState.timeout);
+        internalState.lastUpdate = undefined;
+        state.time = time;
+        internalApi.updateSubscribers({ type: "setTime" });
+        internalApi.update();
+    };
+
+const createApiGetIsPlaying: CreateApiFn<"getIsPlaying"> =
+    (state, _internalState, _internalApi) => () =>
+        state.isPlaying;
+
+const createApiSubscribe: CreateApiFn<"subscribe"> =
+    (_state, internalState, _internalApi) => (subscriber) => {
+        const idx = internalState.nextSubscriberIdx++;
+        internalState.subscribers[idx] = subscriber;
+        return () => delete internalState.subscribers[idx];
+    };
 
 /**
  * Call this function to initialize a new `Timer`.
@@ -154,107 +129,25 @@ export function createTimer(opts?: Partial<TimerOptions>): Timer {
         ...opts,
     };
 
+    const state = createState();
     const internalState = createInternalState();
+    const internalApi = createInternalApi(state, internalState, options);
 
-    const createTimeout = () => setTimeout(update, options.updateInterval);
-
-    const play = () => {
-        if (timer.isPlaying || timer.isFinished) {
-            return false;
-        }
-
-        timer.isPlaying = true;
-        internalState.timeout !== undefined &&
-            clearTimeout(internalState.timeout);
-        internalState.timeout = createTimeout();
-        updateTime();
-        updateSubscribers({ type: "play" });
-
-        return true;
-    };
-
-    const pause = () => {
-        if (!timer.isPlaying || timer.isFinished) {
-            return false;
-        }
-
-        timer.isPlaying = false;
-        internalState.timeout !== undefined &&
-            clearTimeout(internalState.timeout);
-        updateTime();
-        updateSubscribers({ type: "pause" });
-        internalState.lastUpdate = undefined;
-
-        return true;
-    };
-
-    const togglePlay = () => (timer.isPlaying ? timer.pause() : timer.play());
-
-    const reset = () => {
-        internalState.timeout !== undefined &&
-            clearTimeout(internalState.timeout);
-        internalState.lastUpdate = undefined;
-        timer.time = 0;
-        timer.isPlaying = false;
-        timer.isFinished = false;
-        updateSubscribers({ type: "reset" });
-    };
-
-    const getTime = () => timer.time;
-
-    const setTime = (time: number) => {
-        internalState.timeout !== undefined &&
-            clearTimeout(internalState.timeout);
-        internalState.lastUpdate = undefined;
-        timer.time = time;
-        updateSubscribers({ type: "setTime" });
-        update();
-    };
-
-    const getIsPlaying = () => timer.isPlaying;
-
-    const subscribe: TimerApiSubscribe = (subscriber) => {
-        const idx = internalState.nextSubscriberIdx++;
-        internalState.subscribers[idx] = subscriber;
-        return () => delete internalState.subscribers[idx];
-    };
-
-    const update = () => {
-        updateTime();
-        updateIsFinished();
-        updateSubscribers({ type: timer.isFinished ? "finish" : "update" });
-
-        internalState.timeout !== undefined &&
-            clearTimeout(internalState.timeout);
-        if (!timer.isFinished && timer.isPlaying) {
-            internalState.timeout = createTimeout();
-        }
-    };
-
-    const updateTime = () => {
-        const lastUpdate = internalState.lastUpdate ?? new Date().getTime();
-        const now = new Date().getTime();
-        const diff = now - lastUpdate;
-        timer.time += diff;
-        internalState.lastUpdate = now;
-    };
-
-    const updateIsFinished = () => {
-        const wasFinished = timer.isFinished;
-        timer.isFinished =
-            options.duration === "infinite"
-                ? false
-                : timer.time >= options.duration;
-        if (timer.isFinished && timer.isFinished !== wasFinished) {
-            timer.isPlaying = false;
-        }
-    };
-
-    const updateSubscribers = (event: TimerEvent) => {
-        for (const subscriber of Object.values(internalState.subscribers)) {
-            subscriber(timer, event);
-        }
-    };
+    const play = createApiPlay(state, internalState, internalApi);
+    const pause = createApiPause(state, internalState, internalApi);
+    const togglePlay = createApiTogglePlay(state, internalState, internalApi, {
+        play,
+        pause,
+    });
+    const reset = createApiReset(state, internalState, internalApi);
+    const getTime = createApiGetTime(state, internalState, internalApi);
+    const setTime = createApiSetTime(state, internalState, internalApi);
+    const getIsPlaying = createApiGetIsPlaying(
+        state,
+        internalState,
+        internalApi,
+    );
+    const subscribe = createApiSubscribe(state, internalState, internalApi);
 
     const api: TimerApi = {
         getTime,
@@ -267,10 +160,13 @@ export function createTimer(opts?: Partial<TimerOptions>): Timer {
         subscribe,
     };
 
-    const timer: Timer = {
-        ...createState(),
-        ...api,
-    };
+    // const timer: Timer = {
+    //     ...state,
+    //     ...api,
+    // };
+    const timer: Timer = Object.assign(state, api);
+
+    internalState.timer = timer;
 
     return timer;
 }
@@ -289,5 +185,68 @@ function createInternalState(): TimerInternalState {
         lastUpdate: undefined,
         subscribers: [],
         nextSubscriberIdx: 0,
+        timer: undefined,
     };
+}
+
+function createInternalApi(
+    state: TimerState,
+    internalState: TimerInternalState,
+    options: TimerOptions,
+): TimerInternalApi {
+    const createTimeout = () => setTimeout(update, options.updateInterval);
+
+    const updateTime = () => {
+        const lastUpdate = internalState.lastUpdate ?? new Date().getTime();
+        const now = new Date().getTime();
+        const diff = now - lastUpdate;
+        state.time += diff;
+        internalState.lastUpdate = now;
+    };
+
+    const updateIsFinished = () => {
+        const wasFinished = state.isFinished;
+        state.isFinished =
+            options.duration === "infinite"
+                ? false
+                : state.time >= options.duration;
+        if (state.isFinished && state.isFinished !== wasFinished) {
+            state.isPlaying = false;
+        }
+    };
+
+    const updateSubscribers = (event: TimerEvent) => {
+        if (!internalState.timer) {
+            throw new Error(
+                "[timesub] Internal state doesn't have its Timer! " +
+                    "Can't call subscriber functions without timer object. " +
+                    "This is a `timesub` bug.",
+            );
+        }
+
+        for (const subscriber of Object.values(internalState.subscribers)) {
+            subscriber(internalState.timer, event);
+        }
+    };
+
+    const update = () => {
+        updateTime();
+        updateIsFinished();
+        updateSubscribers({ type: state.isFinished ? "finish" : "update" });
+
+        internalState.timeout !== undefined &&
+            clearTimeout(internalState.timeout);
+        if (!state.isFinished && state.isPlaying) {
+            internalState.timeout = internalApi.createTimeout();
+        }
+    };
+
+    const internalApi: TimerInternalApi = {
+        createTimeout,
+        update,
+        updateTime,
+        updateSubscribers,
+    };
+
+    return internalApi;
 }
