da409a187ee80eb0bd6bb018f3c45be3a47be595
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-06-08 20:52:41 +0200

    Add unit test to events.test.ts

--- tests/events.test.ts
+++ tests/events.test.ts
@@ -19,6 +19,32 @@ describe("listen to timer events with `on` API", () => {
         timer.play();
     });
 
+    it("listens to play and pause events", (done) => {
+        const timer = createTimer();
+
+        const callbackPlay = jest.fn(
+            ({ isPlaying }: Timer, _event: TimerEventOfType<"play">) => {
+                expect(isPlaying).toBe(true);
+                expect(callbackPlay).toHaveBeenCalledTimes(1);
+            },
+        );
+        const unsubscribePlay = timer.on("play", callbackPlay);
+
+        const callbackPause = jest.fn(
+            ({ isPlaying }: Timer, _event: TimerEventOfType<"pause">) => {
+                expect(isPlaying).toBe(false);
+                expect(callbackPause).toHaveBeenCalledTimes(1);
+                unsubscribePlay();
+                unsubscribePause();
+                done();
+            },
+        );
+        const unsubscribePause = timer.on("pause", callbackPause);
+
+        timer.play();
+        timer.pause();
+    });
+
     it("listens to update event", (done) => {
         const timer = createTimer({
             duration: 100,
