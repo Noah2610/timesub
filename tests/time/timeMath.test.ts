1f2ff0bc23272829fd2faea1c4faf68e8f46f4a4
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-06-09 21:08:04 +0200

    Split time unit tests into timeConversion and timeMath files

--- tests/time/timeMath.test.ts
+++ tests/time/timeMath.test.ts
@@ -1,48 +1,5 @@
-import { Time, TimeObj, TimeMs } from "../src/types";
-import { timeToMs, timeToObj, timeMath } from "../src/internal/util";
-
-const expectedTime: { obj: TimeObj; ms: TimeMs } = {
-    obj: {
-        ms: 500,
-        s: 30,
-        m: 20,
-        h: 2,
-    },
-    /*
-    ms =
-        500 +               // ms
-        30 * 1000 +         // s
-        20 * 60 * 1000 +    // m
-        2 * 60 * 60 * 1000  // h
-    */
-    ms: 8430500,
-};
-
-describe("time number <-> object conversion", () => {
-    it("converts number to object", () => {
-        const time = expectedTime.ms;
-        const timeObj = timeToObj(time);
-        expect(timeObj).toEqual(expectedTime.obj);
-    });
-
-    it("converts object to number", () => {
-        const time = expectedTime.obj;
-        const timeMs = timeToMs(time);
-        expect(timeMs).toEqual(expectedTime.ms);
-    });
-
-    it("converts number to number (no change)", () => {
-        const time = expectedTime.ms;
-        const timeMs = timeToMs(time);
-        expect(timeMs).toEqual(expectedTime.ms);
-    });
-
-    it("converts object to object (no change)", () => {
-        const time = expectedTime.obj;
-        const timeObj = timeToObj(time);
-        expect(timeObj).toEqual(expectedTime.obj);
-    });
-});
+import { TimeObj, TimeMs } from "../../src/types";
+import { timeToObj, timeMath } from "../../src/internal/util";
 
 const exampleTimeA: { obj: TimeObj; ms: TimeMs } = {
     obj: {
