262ca4de83390133b755f8add541b584fc1f4495
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-06-25 19:26:11 +0200

    Add test to test direction: backward and duration: infinite

--- tests/direction.test.ts
+++ tests/direction.test.ts
@@ -60,4 +60,39 @@ describe("timer direction", () => {
             done();
         });
     });
+
+    it("plays timer with backward direction and infinite duration", (done) => {
+        const timer = createTimer({
+            duration: "infinite",
+            updateInterval: 10,
+            direction: "backward",
+        });
+
+        expect(timer.getTime()).toEqual(0);
+
+        let timesTested = 0;
+        const maxTimesTested = 3;
+
+        let prevTime: Time = timer.getTime();
+        const testNewTime = jest.fn((newTime: Time) => {
+            expect(newTime).toBeLessThan(0);
+            expect(newTime).toBeLessThan(prevTime);
+        });
+
+        const unsubscribe = timer.subscribe(({ time, pause }) => {
+            if (time === prevTime) {
+                return;
+            }
+            testNewTime(time);
+            prevTime = time;
+            if (++timesTested >= maxTimesTested) {
+                unsubscribe();
+                pause();
+                expect(testNewTime).toHaveBeenCalled();
+                done();
+            }
+        });
+
+        timer.play();
+    });
 });
